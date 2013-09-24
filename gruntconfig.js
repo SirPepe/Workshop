933ab37cbe5e9b94e101f984960b01a681288b93
RudiDutschke <thomas.lietzow@tns-infratest.com>
2013-09-24 17:03:34 +0200

    gruntfile.js und gruntconfig.js
    Paths für Module ausgelagert um alle Module für die beiden Apps zu laden

---
+++ gruntconfig.js
@@ -0,0 +1,6 @@
+module.exports.paths = {
+          requirejs: '../bower_components/requirejs/require',
+          jquery: '../bower_components/jquery/jquery',
+          chart: '../bower_components/nnnick-chartjs/Chart.min',
+          text: '../bower_components/requirejs-text/text'
+        };
