05240c3b28902d06b63cea844be4c6a53e07077f
Peter Kröner <peter@peterkroener.de>
2013-09-25 19:15:39 +0200

    Pofalla-Problem sollte gegessen sein. Außerdem neu: once(topic, cb)

--- src/mixins/EventEmitter.js
+++ src/mixins/EventEmitter.js
@@ -1,5 +1,6 @@
 // Ganz simpler Event Emitter als Mixin. Stellt folgende Funktionen bereit:
 //  * `on(topic, cb)` setzt einen Callback "cb" als Listener auf das Event "topic" an
+//  * `once(topic, cb)` setzt "cb" als einmaligen Listener auf "topic" an
 //  * `off(topic, cb)` entfernt einen Callback als Listener
 //  * `trigger(topic, data...)` löst das Event "topic" aus und übergibt ggf. weitere Daten
 // Benutzung: EventEmitter.call(MeinObjConstructor.prototype);
@@ -7,10 +8,10 @@ define(function(){
 
   "use strict";
 
-  var removeFromArray = function(arr, from, to){
-    var rest = arr.slice((to || from) + 1 || arr.length);
-    arr.length = from < 0 ? arr.length + from : from;
-    return arr.push.apply(arr, rest);
+  var cleanup = function(arr){
+    arr = arr.filter(function(value){
+      return typeof value !== 'undefined';
+    });
   };
 
   return function EventEmitter(){
@@ -24,22 +25,32 @@ define(function(){
       topics[topic].push(callback);
     };
 
+    // Meldet "callback" als Event Handler für "topic" an, entfernt
+    // "callback" nach einmaliger Verwendung wieder
+    this.once = function(topic, callback){
+      var self = this;
+      if(typeof topics[topic] === 'undefined') topics[topic] = [];
+      topics[topic].push(function fn(){
+        callback.apply(null, arguments);
+        self.off(topic, fn);
+      });
+    };
+
     // Entfernt "callback" für das Event "topic"
     this.off = function(topic, callback){
       var index = topics[topic].indexOf(callback);
-      if(index > -1){
-        removeFromArray(topics[topic], index);
-      }
+      if(index > -1) delete topics[topic][index];
     };
 
     // Löst alle Callbacks für das Event "topic" aus und übergibt alle weiteren
     // an "trigger" übergebenen Parameter als Daten-Arguments an die Callbacks
     this.trigger = function(topic){
       if(typeof topics[topic] === 'undefined') return;
+      cleanup(topics[topic]);
       var args = Array.prototype.slice.call(arguments, 1);
       for(var i = 0; i < topics[topic].length; i++){
         var callback = topics[topic][i];
-        callback.apply(null, args);
+        if(callback) callback.apply(null, args);
       }
     };
 
