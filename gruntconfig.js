78eb08da184f454c319e1fc1d17d6bb4aaa6b42e
juergen grosse-ophoff <jayson@web.de>
2013-09-25 10:19:26 +0200

    underscore.js eingebaut

--- gruntconfig.js
+++ gruntconfig.js
@@ -2,5 +2,6 @@ module.exports.paths = {
           requirejs: '../bower_components/requirejs/require',
           jquery: '../bower_components/jquery/jquery',
           chart: '../bower_components/nnnick-chartjs/Chart.min',
-          text: '../bower_components/requirejs-text/text'
+          text: '../bower_components/requirejs-text/text',
+          underscore: '../bower_components/underscore/underscore'
         };
