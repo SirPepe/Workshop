ad03eb9e365a5d1ee771626dfbb3806e3fff0479
RudiDutschke <thomas.lietzow@tns-infratest.com>
2013-09-25 11:57:57 +0200

    Navigation angefangen und in ausgabe.js erste Version eingebunden
    Skip durch pages mit verschiedenen Widgets

--- src/ausgabe.js
+++ src/ausgabe.js
@@ -7,9 +7,30 @@ require([
   'lib/LoadWidget',
   'widgets/ChartWidget/ChartWidget',
   'widgets/TextWidget/TextWidget'
-], function(_require, $, Mediator, loadWidget){
+], function(_require, $, Mediator, loadWidget,
+  ChartWidget,
+  TextWidget
+){
 
   'use strict';
+  
+  var pages = {
+    seite1: [ChartWidget],
+    seite2: [TextWidget]
+  };
+  
+  
+  
+  function pageLoader(pages){  
+    var page = window.location.hash.substring(1);
+    if (pages.hasOwnProperty(page)) return pages[page];
+    return pages.seite1; 
+  }
+  
+ 
+  
+  //var b = pageLoader(pages);
+  //alert(b);
 
   // Globalen App-Namespace initialisieren. Alle globalen Variablen werden, wenn
   // sie denn nötig sind, hier platziert.
@@ -24,6 +45,23 @@ require([
 
   // Jedes Widget ist ein Mini-Programm und muss einzeln initialisiert werden
   var loadWidgetsInMain = loadWidget.bind(null, $main);
-  [].slice.call(arguments, 4).forEach(loadWidgetsInMain);
+  
+  
+  //[].slice.call(arguments, 5).forEach(loadWidgetsInMain);
+  //pageLoader(pages).forEach(loadWidgetsInMain);
+  
+  //window.addEventListener('hashchange', function(){
+  //  alert('EventListener' + window.location.hash);
+  //  pageLoader(pages).forEach(loadWidgetsInMain);
+  //}, false);
+  
+  var init = function(){
+    pageLoader(pages).forEach(loadWidgetsInMain);
+  };
+  $(window).on('hashchange', init);
+  $(document).ready(init);
+  
+  
+  
 
 });
