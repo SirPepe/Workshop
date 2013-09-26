974e7e9ec04c8c80d777482043dd896c8d0b8aca
GerdDoering <gerd.doering@tns-infratest.com>
2013-09-26 13:49:47 +0200

    Q2 - Widget erstellt für Altersfrage
    Erweiterung des Fragebogens

---
+++ src/widgets/Q2Widget/Q2Widget.js
@@ -0,0 +1,25 @@
+﻿// Q2 NUMBER-Question for age
+
+define(['jquery', 'text!widgets/Q2Widget/form.html'], function($,html){
+  'use strict';
+
+  // Die übliche Modul-Constructor-Funktion
+  return function Q2Widget(target){
+     $(target).html(html);
+     var $form = $(target).find("form");
+     $form.submit(function(evt){
+         evt.preventDefault();
+         var val = $(target).find("input[name='age']").val();
+         window.APP.mediator.trigger('q2result', val);
+    });
+    
+     window.APP.mediator.on('q2data');
+
+//Pofalla beendet Target
+    window.APP.mediator.on("pofalla", function listener(){
+       $(target).remove();
+       window.APP.mediator.off("pofalla", listener);
+       window.APP.mediator.off("q2data");
+    });
+  };
+});
