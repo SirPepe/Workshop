06e7cd9d9827b91d23bfd52c0156d7dd3627fcbb
RudiDutschke <thomas.lietzow@tns-infratest.com>
2013-09-26 10:51:22 +0200

    fragebogen.js erzeugt einen Next Button
    Next Button springt von seite1 auf seite2. Am Ende wird ein alert
    erzeugt.

--- src/fragebogen.js
+++ src/fragebogen.js
@@ -16,8 +16,8 @@ require([
   'use strict';
   
   var pages = {
-    seite1: [Q1Widget, VideoWidget],
-    seite2: [TextWidget]
+    seite1: [Q1Widget],
+    seite2: [VideoWidget]
   };
 
   function pageLoader(pages){  
@@ -36,15 +36,42 @@ require([
 
   // Container-Element für alle Widgets
   $main = $('main');
+  
+  // Footer-Element für next button
+  $footer = $('footer');
+  
+  // Next Button
+   var createButton = function(){
+    return $('<input/>').attr({
+      type: 'button',
+      class: 'btn btn-primary clearfix',
+    });};
+    
+  var $nextbutton=createButton();
+  $nextbutton.addClass('next').val('NEXT');
  
   // Jedes Widget ist ein Mini-Programm und muss einzeln initialisiert werden
   var loadWidgetsInMain = loadWidget.bind(null, $main);
   
+  //Sendet Pofalla
   var init = function(){
     window.APP.mediator.trigger('pofalla');
     pageLoader(pages).forEach(loadWidgetsInMain);
+    $nextbutton.prependTo($footer);
   };
   $(window).on('hashchange', init);
-  $(document).ready(init);
+  $(document).ready(init); 
+  
+  $nextbutton.click(function(){
+    var keys = Object.keys(pages);
+    var index = keys.indexOf(window.location.hash.substring(1));
+    var next = keys[index + 1];
+    if(typeof next !== 'undefined'){
+      window.location.hash='#' + next;
+    }
+    else {
+      alert('Danke für das Ausfüllen');
+    }
+  });
 
 });
